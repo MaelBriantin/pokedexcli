6543e2fbc2e858b7723a03adad83736433636b6b
MaelBriantin <mael.briantin@proton.me>
2025-10-05 10:58:12 +0200

    feat: refactor location details API and add tests for caching

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -27,57 +27,14 @@ type PokemonEncounter struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"pokemon"`
-	VersionDetails []struct {
-		EncounterDetails []struct {
-			Chance          int   `json:"chance"`
-			ConditionValues []any `json:"condition_values"`
-			MaxLevel        int   `json:"max_level"`
-			Method          struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"method"`
-			MinLevel int `json:"min_level"`
-		} `json:"encounter_details"`
-		MaxChance int `json:"max_chance"`
-		Version   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"version"`
-	} `json:"version_details"`
 }
 
 type LocationDetailsPokeAPIResponse struct {
-	EncounterMethodRates []struct {
-		EncounterMethod struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"encounter_method"`
-		VersionDetails []struct {
-			Rate    int `json:"rate"`
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-		} `json:"version_details"`
-	} `json:"encounter_method_rates"`
-	GameIndex int `json:"game_index"`
-	ID        int `json:"id"`
-	Location  struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"location"`
-	Name  string `json:"name"`
-	Names []struct {
-		Language struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
-		Name string `json:"name"`
-	} `json:"names"`
 	PokemonEncounters []PokemonEncounter `json:"pokemon_encounters"`
 }
 
 var cache = pokecache.NewCache(10 * time.Minute)
+var baseURL = "https://pokeapi.co/api/v2" // Ajouter cette variable
 
 func locationJsonDecode(data []byte, response *LocationPokeAPIResponse) LocationPokeAPIResponse {
 	if err := json.Unmarshal(data, response); err != nil {
@@ -99,7 +56,6 @@ func GetLocationAreas(url string) LocationPokeAPIResponse {
 		response := LocationPokeAPIResponse{}
 		return locationJsonDecode(data, &response)
 	}
-
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error fetching locations:", err)
@@ -107,32 +63,26 @@ func GetLocationAreas(url string) LocationPokeAPIResponse {
 	}
 	data, err := io.ReadAll(res.Body)
 	res.Body.Close()
-
 	// Store in cache
 	cache.Add(url, data)
-
 	return locationJsonDecode(data, &LocationPokeAPIResponse{})
 }
 
 func GetLocationDetails(location string) LocationDetailsPokeAPIResponse {
-	url := "https://pokeapi.co/api/v2/location-area/" + location
+	url := baseURL + "/location-area/" + location
 	// Check cache first
 	if data, found := cache.Get(url); found {
 		response := LocationDetailsPokeAPIResponse{}
 		return encounterJsonDecode(data, &response)
 	}
-
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("Error fetching location details:", err)
 		return LocationDetailsPokeAPIResponse{}
 	}
-
 	data, err := io.ReadAll(res.Body)
 	res.Body.Close()
-
 	// Store in cache
 	cache.Add(url, data)
-
 	return encounterJsonDecode(data, &LocationDetailsPokeAPIResponse{})
 }
--- internal/pokeapi/pokeapi_test.go
+++ internal/pokeapi/pokeapi_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/MaelBriantin/pokedexcli/internal/pokecache"
 )
 
-func fakeAPIResponse() []byte {
+func fakeLocationAPIResponse() []byte {
 	resp := LocationPokeAPIResponse{
 		Count:    1,
 		Next:     "",
@@ -23,9 +23,23 @@ func fakeAPIResponse() []byte {
 	return data
 }
 
+func fakeLocationDetailsAPIResponse() []byte {
+	resp := LocationDetailsPokeAPIResponse{
+		PokemonEncounters: []PokemonEncounter{
+			{Pokemon: struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			}{Name: "test-pokemon", URL: "https://pokeapi.co/api/v2/pokemon/1/"},
+			},
+		},
+	}
+	data, _ := json.Marshal(resp)
+	return data
+}
+
 func TestGetLocationAreas_HTTP(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(fakeAPIResponse())
+		w.Write(fakeLocationAPIResponse())
 	}))
 	defer ts.Close()
 
@@ -39,7 +53,7 @@ func TestGetLocationAreas_HTTP(t *testing.T) {
 
 func TestGetLocationAreas_Cache(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(fakeAPIResponse())
+		w.Write(fakeLocationAPIResponse())
 	}))
 	defer ts.Close()
 
@@ -54,6 +68,54 @@ func TestGetLocationAreas_Cache(t *testing.T) {
 
 	resp2 := GetLocationAreas(ts.URL)
 	if resp2.Count != 1 || len(resp2.Results) != 1 || resp2.Results[0].Name != "test-location" {
+		t.Errorf("unexpected response from cache: %+v", resp2)
+	}
+}
+
+func TestGetLocationDetails_HTTP(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(fakeLocationDetailsAPIResponse())
+	}))
+	defer ts.Close()
+
+	originalBaseURL := baseURL
+	baseURL = ts.URL
+	defer func() { baseURL = originalBaseURL }()
+
+	cache = pokecache.NewCache(10 * time.Minute)
+
+	resp := GetLocationDetails("test-location")
+
+	if len(resp.PokemonEncounters) != 1 {
+		t.Errorf("expected 1 pokemon encounter, got %d", len(resp.PokemonEncounters))
+	}
+
+	if len(resp.PokemonEncounters) > 0 && resp.PokemonEncounters[0].Pokemon.Name != "test-pokemon" {
+		t.Errorf("expected pokemon name 'test-pokemon', got '%s'", resp.PokemonEncounters[0].Pokemon.Name)
+	}
+}
+
+func TestGetLocationDetails_Cache(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(fakeLocationDetailsAPIResponse())
+	}))
+	defer ts.Close()
+
+	originalBaseURL := baseURL
+	baseURL = ts.URL
+	defer func() { baseURL = originalBaseURL }()
+
+	cache = pokecache.NewCache(10 * time.Minute)
+
+	resp1 := GetLocationDetails("test-location")
+	if len(resp1.PokemonEncounters) != 1 || resp1.PokemonEncounters[0].Pokemon.Name != "test-pokemon" {
+		t.Errorf("unexpected first response: %+v", resp1)
+	}
+
+	ts.Close()
+
+	resp2 := GetLocationDetails("test-location")
+	if len(resp2.PokemonEncounters) != 1 || resp2.PokemonEncounters[0].Pokemon.Name != "test-pokemon" {
 		t.Errorf("unexpected response from cache: %+v", resp2)
 	}
 }
